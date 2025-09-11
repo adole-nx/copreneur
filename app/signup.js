690812ec63562f9d08fb93d50b5056ff9c6da12e
Adole Daniel <adoledaniel111@gmail.com>
2025-09-11 11:06:05 +0100

    validation

--- app/signup.js
+++ app/signup.js
@@ -1,14 +1,36 @@
 import { Link } from "expo-router";
-import { useState } from "react";
+import { useFormik } from "formik";
 import { Image, KeyboardAvoidingView, Platform, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import * as yup from "yup";
 import { colors } from "../theme/colors";
 
+const validationRules = yup.object({
+    email: yup.string().email().required(),
+    password: yup
+        .string()
+        .min(8, "Password must be at least 8 characters long.")
+        .matches(/[A-Z]/, "Password must contain at least one uppercase letter.")
+        .matches(/[a-z]/, "Password must contain at least one lowercase letter.")
+        .matches(/\d/, "Password must contain at least one number.")
+        .matches(/[!@#$%^&*]/, "Password must contain at least one special character.")
+        .required("password is required"),
+
+    passwordConfirmation: yup.string()
+        .oneOf([yup.ref("password"), null], "password must match")
+        .required("confirm password is required")
+});
+
 export default function Signup() {
-    // this will capture our email text input as we are typing
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirmation, setPasswordConfirmation] = useState("");
-    console.log("email", email)
+
+    const { handleBlur, handleChange, handleSubmit, touched, errors, values } = useFormik({
+        initialValues: { email: "", password: "", passwordConfirmation: "" },
+        onSubmit: () => {
+            console.log("form was submitted")
+        },
+        validationSchema: validationRules
+    });
+
+    console.log(values.email)
 
     return (
         <KeyboardAvoidingView
@@ -52,37 +74,56 @@ export default function Signup() {
                     </View>
 
                     {/* create account with email and password */}
-                    <View style={styles.emailSec}>
-                        <TextInput
-                            keyboardType="email-address"
-                            style={styles.input}
-                            placeholder="eg. john@example.com"
-                            // assign the current value of the textinput
-                            value={email}
-                            onChangeText={(text) => setEmail(text)} />
+                    <View style={styles.form}>
 
-                        <TextInput
-                            secureTextEntry={true}
-                            keyboardType="default"
-                            style={styles.input}
-                            placeholder="create password"
-                            value={password}
-                            onChangeText={(text) => setPassword(text)} />
+                        <View style={styles.inputBlock}>
+                            <TextInput
+                                keyboardType="email-address"
+                                style={styles.input}
+                                placeholder="eg. john@example.com"
+                                // assign the current value of the textinput
+                                value={values.email}
+                                onChangeText={handleChange("email")}
+                                onBlur={handleBlur("email")}
+                            />
+                            {errors.email && touched.email &&
+                                <Text style={styles.errormsg}>{errors.email}</Text>}
 
-                        {password.length >= 8 &&
+                        </View>
+
+                        <View style={styles.inputBlock}>
                             <TextInput
                                 secureTextEntry={true}
                                 keyboardType="default"
                                 style={styles.input}
-                                placeholder="confirm password"
-                                value={passwordConfirmation}
-                                onChangeText={(text) => setPasswordConfirmation(text)} />}
+                                placeholder="create password"
+                                value={values.password}
+                                onChangeText={handleChange("password")}
+                                onBlur={handleBlur("password")} />
+                            {errors.password && touched.password &&
+                                <Text style={styles.errormsg}>{errors.password}</Text>}
+                        </View>
 
-                        {password.length >= 8 && password == passwordConfirmation &&
-                            <TouchableOpacity style={styles.signupBtn}>
+                        {!errors.password && touched.password &&
+                            <View>
+                                <TextInput
+                                    secureTextEntry={true}
+                                    keyboardType="default"
+                                    style={styles.input}
+                                    placeholder="confirm password"
+                                    value={values.passwordConfirmation}
+                                    onChangeText={handleChange("passwordConfirmation")}
+                                    onBlur={handleBlur("passwordConfirmation")} />
+
+                                {errors.passwordConfirmation && touched.passwordConfirmation &&
+                                    <Text style={styles.errormsg}>{errors.passwordConfirmation}</Text>}
+                            </View>}
+
+                        {!errors.passwordConfirmation && !errors.email &&
+                            touched.passwordConfirmation &&
+                            <TouchableOpacity onPress={handleSubmit} style={styles.signupBtn}>
                                 <Text style={styles.signInText}>Create Account</Text>
-                            </TouchableOpacity>
-                        }
+                            </TouchableOpacity>}
                     </View>
 
                     {/* already have an account? */}
@@ -189,8 +230,8 @@ const styles = StyleSheet.create({
         borderTopWidth: 1,
         borderTopColor: colors.brown300,
     },
-    emailSec: {
-        gap: 8,
+    form: {
+        gap: 12,
     },
     input: {
         borderWidth: 1,
@@ -199,5 +240,9 @@ const styles = StyleSheet.create({
         fontSize: 16,
         paddingHorizontal: 6,
     },
+    errormsg: {
+        color: "red",
+        fontSize: 12
+    }
 
 });
